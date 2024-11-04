6cc669187606d9362a55ac304c0a1b2dd6a3c024
Ishaaq Ahmed <a.ishaaq@icloud.com>
2024-11-04 10:02:20 +0000

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Ishaaq Ahmed on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by Ishaaq Ahmed on 04/11/2024.
+//
+
+import Testing
+@testable import Notes
+
+struct NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
